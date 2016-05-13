e9cc4b88d31244428efe93481a9973929bf4c0bc
obelouix <obelouix25@gmail.com>
2016-05-13 07:49:47 +0200

    récupération de la langue système

--- Electro Games Launcher/I18n.cpp
+++ Electro Games Launcher/I18n.cpp
@@ -1,5 +1,5 @@
 #include "I18n.h"
-
+#include <WinUser.h>
 
 
 I18n::I18n()
@@ -9,4 +9,14 @@ I18n::I18n()
 
 I18n::~I18n()
 {
+}
+
+int GetSystemDefaultLocaleName(
+	_Out_ LPWSTR lpLocaleName,
+	_In_  int    cchLocaleName
+);
+
+int GetSystemDefaultLocaleName(LPWSTR lpLocaleName, int cchLocaleName)
+{
+	return 0;
 }
--- Electro Games Launcher/I18n.h
+++ Electro Games Launcher/I18n.h
@@ -4,5 +4,6 @@ class I18n
 public:
 	I18n();
 	~I18n();
+	int GetSystemDefaultLocaleName(LPWSTR lpLocaleName, int cchLocaleName);
 };
 
